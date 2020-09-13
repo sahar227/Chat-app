648cd320c0d6a66d21e043a5d34a4884f7be68e5
sahar227 <sahar22798@gmail.com>
2020-09-13 13:14:49 +0300

    added mongoose

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,15 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const mongoose = require('mongoose');
+
+mongoose.connect(config.get('mongo.connection_string'), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
 app.get('/test', (req, res) => {
     return res.send('test');
 });
