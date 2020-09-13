ef5a3c958b05fa3df77617a744c6cff5fda86561
sahar227 <sahar22798@gmail.com>
2020-09-13 17:59:14 +0300

    Started working on header component

---
+++ src/client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="header-container">
+            CHAT-APP
+            <li>second</li>
+            <li>third</li>
+        </div>
+    )
+}
